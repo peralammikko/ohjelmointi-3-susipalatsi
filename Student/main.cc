c8c365af8c6c3179682b7d799f1d3b2639e1070e
Mikko Perälä <mikko.perala@tuni.fi>
2021-03-26 16:48:58 +0200

    Show mainwindow

--- Student/main.cc
+++ Student/main.cc
@@ -1,9 +1,13 @@
+#include "simplemainwindow.hh"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Q_INIT_RESOURCE(default_assets);
+    SimpleMainWindow w;
+    w.show();
 
     return a.exec();
 }
