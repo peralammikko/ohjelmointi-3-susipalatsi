f2ef3a5ed492c3a8cae429cefe02a46a0c8ec053
Vilho Hirvonen <vilho.hirvonen@tuni.fi>
2021-03-30 19:45:33 +0300

    Game class and players on launch

--- Student/main.cc
+++ Student/main.cc
@@ -1,4 +1,7 @@
 #include "simplemainwindow.hh"
+#include "game.cpp"
+#include <QtDebug>
+#include <string>
 
 #include <QApplication>
 
@@ -6,7 +9,24 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Q_INIT_RESOURCE(default_assets);
+
+    // Alustetaan peliluokka
+    std::shared_ptr<Interface::Game> game(nullptr);
+    game = make_shared<Interface::Game>();
+
+    // luodaan demonstroinniksi 3 pelaajaa
+    for (int i=0; i<3; i++){
+        // annettu merkkijono on vain pelaajanimi, sen voisi syöttää myöhemmin jotenkin
+        game->addPlayer("a");
+    }
+
     SimpleMainWindow w;
+
+    // tämä kohta piirtää kortteja ja slotteja pöydälle. Ei vielä tarpeellinen.
+    // w.drawSlots();        i;
+    // w.drawCardToSlot();
+    // SimpleCardItem* card = w.drawDeck(0, 1);
+
     w.show();
 
     return a.exec();
