4e4c154c66175870422b3039eee1735455b793c9
Mikko Perälä <mikko.perala@tuni.fi>
2021-04-01 16:30:44 +0300

    Adding and drawing locations

--- Student/main.cc
+++ Student/main.cc
@@ -5,22 +5,39 @@
 
 #include <QApplication>
 
+enum PAIKKANIMET {
+    Marketti,
+    Kirkko,
+    Taverna,
+    Kauppiaiden_kilta,
+    Menomesta,
+    Salapaikka
+};
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Q_INIT_RESOURCE(default_assets);
 
     // Alustetaan peliluokka
-    std::shared_ptr<Interface::Game> game(nullptr);
-    game = make_shared<Interface::Game>();
+    std::shared_ptr<Interface::Game> gameboard(nullptr);
+    gameboard = make_shared<Interface::Game>();
 
     // luodaan demonstroinniksi 3 pelaajaa
     for (int i=0; i<3; i++){
         // annettu merkkijono on vain pelaajanimi, sen voisi syöttää myöhemmin jotenkin
-        game->addPlayer("a");
+        gameboard->addPlayer("a");
+    }
+    std::vector<QString> paikat = {"Marketti", "Kirkko", "Taverna", "Kauppiaiden kilta", "Menomesta", "Salapaikka"};
+
+    for (int i = 0; i < 6; i++) {
+        std::shared_ptr<Interface::Location> location = std::make_shared<Interface::Location>(i, paikat.at(i));
+        gameboard->addLocation(location);
     }
 
     SimpleMainWindow w;
+
+    w.drawLocations(2, 3, paikat);
 
     // tämä kohta piirtää kortteja ja slotteja pöydälle. Ei vielä tarpeellinen.
     // w.drawSlots();        i;
