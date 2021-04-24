07a6b12607815b60e36a12f355806b9d2917be2e
Vilho Hirvonen <vilho.hirvonen@tuni.fi>
2021-04-25 00:53:11 +0300

    Greatly improved AI code readability, first try at Doxygen commenting or whatever it is

--- Student/aicontrol.cpp
+++ Student/aicontrol.cpp
@@ -6,18 +6,17 @@ namespace Interface {
 
 AiControl::AiControl(GameScene *gameScene, std::shared_ptr<Player> player) : gameScene_(gameScene), player_(player)
 {
-
+    hand_ =  gameScene_->playerHands().at(player_);
 }
 
 std::shared_ptr<ActionInterface> AiControl::nextAction()
 {
-     auto hand = gameScene_->playerHands().at(player_);
-     auto actioncards = hand->getCardItems();
-
+     action_ = nullptr;
+     auto actioncards = hand_->getCardItems();
      Q_ASSERT(actioncards.size() > 0);
-     std::shared_ptr<ActionInterface> action = ponderActions();
-     if (!action.get()){
-         return  std::make_shared<PassAction>(hand);
+     ponderActions();
+     if (!action_.get()){
+         return  std::make_shared<PassAction>(hand_);
      } else {
          auto paymentcard = actioncards.at(0);
          player_->playCard(paymentcard->getCard());
@@ -26,97 +25,32 @@ std::shared_ptr<ActionInterface> AiControl::nextAction()
              location->discards().get()->addCard(paymentcard->getCard());
          }
          delete paymentcard;
-         hand->rearrange();
-         return action;
+         hand_->rearrange();
+         qDebug() << "12342423";
+         return action_;
      }
 }
 
-std::shared_ptr<ActionInterface> AiControl::ponderActions()
+void AiControl::ponderActions()
 {
     Q_ASSERT(gameScene_ != nullptr);
-    auto hand = gameScene_->playerHands().at(player_);
+    Q_ASSERT(hand_ != nullptr);
+    hand_ = gameScene_->playerHands().at(player_);
     std::shared_ptr<ActionInterface> action;
 
-    auto handAgents = hand->getAgentItems();
-    auto locItems = gameScene_->GetLocItems();
+    auto handAgents = hand_->getAgentItems();
+    locItems_ = gameScene_->GetLocItems();
     // These aitems are in locations
-    std::vector<agentItem*> aitems =  gameScene_->getAgentItemsForPlayer(player_);
+    aitems_ =  gameScene_->getAgentItemsForPlayer(player_);
 
-    // The AI prioritizes playing agents from its hand
-    if (handAgents.size()){
-        unsigned int randomLocIndex = Interface::Random::RANDOM.uint(locItems.size()-1);
-        action = std::make_shared<SendAgentAction>(locItems.at(randomLocIndex), handAgents.at(0));
-        if (action->canPerform()){
-            return action;
-        }
-    }
-    for (auto aitem : aitems){
-        auto agent = aitem->getAgentClass();
-        auto playercards = player_->cards();
-        // find the locitem where the aitem is
-        LocationItem* homeItem = nullptr;
-        for (auto locItem : locItems){
-            if (aitem->parentItem() == locItem){
-                homeItem = locItem;
-                break;
-            }
-        }
-        if (!homeItem){
-            continue;
-        }
-        // Check if agent can get councilor card in its home location
-        // TODO: add influence to the check
-        if (canGetCounilorCard(aitem, homeItem)){
-            getCouncilorCard(aitem, homeItem);
-        }
-
-        // Returning councilor cards takes second highest priority
-        if (aitem->getAgentClass()->hasCouncilCard()) {
-            // The way how councilcards work is that the agent keeps on carrying it, apparently.
-            // To prevent returning the card multiple times, check if the player already has that card.
-            auto councilcardFind = std::find(playercards.begin(), playercards.end(), aitem->getAgentClass()->getCouncilCard());
-            if (councilcardFind == playercards.end()){
-                action = std::make_shared<WithdrawAgentAction>(hand, aitem);
-                if (action->canPerform()){
-                    return action;
-                }
-            }
-        }
-        // Moving to a location in which Councilor demands items which the agent has is on third highest priority
-        auto neighbours = homeItem->neighbours();
-        if (canGetCounilorCard(aitem, neighbours.first)){
-            action = std::make_shared<SendAgentAction>(neighbours.first, aitem);
-            if (action->canPerform()){
-                return action;
-            }
-        }
-        if (canGetCounilorCard(aitem, neighbours.second)){
-            action = std::make_shared<SendAgentAction>(neighbours.second, aitem);
-            if (action->canPerform()){
-                return action;
-            }
-        }
-
-        // Agent has a small chance to be moved to a neighbouring location.
-        // This chance increases as the agent's influence in the area increases (the agent gets bored!)
-        int influence = homeItem->getObject()->influence(player_);
-        unsigned int rand = Interface::Random::RANDOM.uint(std::max(10-influence, 2));
-        if (rand == 0){
-            action = std::make_shared<SendAgentAction>(neighbours.first, aitem);
-            if (action->canPerform()){
-                return action;
-            }
-        } else if (rand == 1) {
-            if (canGetCounilorCard(aitem, neighbours.second)){
-                action = std::make_shared<SendAgentAction>(neighbours.second, aitem);
-                if (action->canPerform()){
-                    return action;
-                }
-            }
-        }
+    for (unsigned int i = 0; i < considerationsVector.size(); ++i){
+         if (action_.get() == nullptr){
+            (this->*considerationsVector[i])();
+         } else {
+             return;
+         }
     }
 
-    return action;
 }
 
 bool AiControl::canGetCounilorCard(agentItem *aitem, LocationItem *locItem)
@@ -147,7 +81,116 @@ bool AiControl::canGetCounilorCard(agentItem *aitem, LocationItem *locItem)
     }
 }
 
- void AiControl::getCouncilorCard(agentItem *aitem, LocationItem *locItem)
+int AiControl::considerSendingFromHand()
+{
+    Q_ASSERT(gameScene_ != nullptr);
+    Q_ASSERT(hand_ != nullptr);
+    qDebug() << "Hmm!";
+    auto handAgents = hand_->getAgentItems();
+    auto locItems = gameScene_->GetLocItems();
+    // These aitems are in locations
+    std::vector<agentItem*> aitems =  gameScene_->getAgentItemsForPlayer(player_);
+    // The AI prioritizes playing agents from its hand
+    if (handAgents.size()){
+        unsigned int randomLocIndex = Interface::Random::RANDOM.uint(locItems.size()-1);
+        auto ac = std::make_shared<SendAgentAction>(locItems.at(randomLocIndex), handAgents.at(0));
+        if (ac->canPerform()){
+            action_ = ac;
+            return 1;
+        }
+    }
+    return 1;
+}
+
+int AiControl::considerWithdrawing()
+{
+    Q_ASSERT(hand_ != nullptr);
+    for (auto aitem : aitems_){
+        auto agent = aitem->getAgentClass();
+        auto playercards = player_->cards();
+        // find the locitem where the aitem is
+        LocationItem* homeItem = findHomeItemFor(aitem);
+        if (!homeItem){
+            continue;
+        }
+        if (aitem->getAgentClass()->hasCouncilCard()) {
+            // The way how councilcards work is that the agent keeps on carrying it, apparently.
+            // To prevent returning the card multiple times, check if the player already has that card.
+            auto councilcardFind = std::find(playercards.begin(), playercards.end(), aitem->getAgentClass()->getCouncilCard());
+            if (councilcardFind == playercards.end()){
+                auto ac = std::make_shared<WithdrawAgentAction>(hand_, aitem);
+                if (ac->canPerform()){
+                    action_ = ac;
+                    return 1;
+                }
+            }
+        }
+    }
+}
+
+int AiControl::considerSmartMoving()
+{
+    for (auto aItem : aitems_){
+        auto homeItem = findHomeItemFor(aItem);
+        Q_ASSERT(homeItem != nullptr);
+        // Moving to a location in which Councilor demands items which the agent has is on third highest priority
+        auto neighbours = homeItem->neighbours();
+        if (canGetCounilorCard(aItem, neighbours.first)){
+            auto ac = std::make_shared<SendAgentAction>(neighbours.first, aItem);
+            if (ac->canPerform()){
+                action_ = ac;
+                return 1;
+            }
+        }
+        if (canGetCounilorCard(aItem, neighbours.second)){
+            auto ac = std::make_shared<SendAgentAction>(neighbours.second, aItem);
+            if (ac->canPerform()){
+                action_ = ac;
+                return 1;
+            }
+        }
+    }
+
+}
+
+int AiControl::considerRandomMoving()
+{
+    for (auto aItem : aitems_){
+        auto homeItem = findHomeItemFor(aItem);
+        Q_ASSERT(homeItem != nullptr);
+        auto neighbours = homeItem->neighbours();
+        int influence = homeItem->getObject()->influence(player_);
+        unsigned int rand = Interface::Random::RANDOM.uint(std::max(10-influence, 2));
+        if (rand == 0){
+            auto ac = std::make_shared<SendAgentAction>(neighbours.first, aItem);
+            if (ac->canPerform()){
+                action_ = ac;
+                return 1;
+            }
+        } else if (rand == 1) {
+            if (canGetCounilorCard(aItem, neighbours.second)){
+                auto ac = std::make_shared<SendAgentAction>(neighbours.second, aItem);
+                if (ac->canPerform()){
+                    action_ = ac;
+                    return 1;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+LocationItem* AiControl::findHomeItemFor(agentItem *aItem)
+{
+    for (auto locItem : locItems_){
+        if (aItem->parentItem() == locItem){
+            return locItem;
+        }
+    }
+    return nullptr;
+}
+
+void AiControl::getCouncilorCard(agentItem *aitem, LocationItem *locItem)
 {
     auto neededRes = locItem->getDemandedResource();
     int reqAmount = neededRes.amount();
--- Student/aicontrol.hh
+++ Student/aicontrol.hh
@@ -13,6 +13,7 @@ namespace Interface {
 
 class AiControl : public ControlInterface
 {
+    typedef void (AiControl::*somefunc)();
 public:
 
     explicit AiControl(GameScene* gameScene, std::shared_ptr<Player> player);
@@ -26,10 +27,13 @@ public:
 private:
     // Calculates or randomizes a move for one of its agents. By default it returns a pass action.
     // If any of the agents can get a Councilor card in its location item, it also gathers that card.
-    std::shared_ptr<ActionInterface> ponderActions();
+    void ponderActions();
 
     // Checks if one of AI's agents is legible to get a councilor card in its current placement.
     bool canGetCounilorCard(agentItem *aitem, LocationItem* locItem);
+
+
+    LocationItem* findHomeItemFor(agentItem* aItem);
 
     // Pre: agent can get councilor card from location and is in that location
     // Post: agent has councilor card of locItem
@@ -38,6 +42,49 @@ private:
     std::shared_ptr<ActionInterface> action_;
     GameScene* gameScene_;
     std::shared_ptr<Player> player_;
+    PlayerHand* hand_;
+    std::vector<agentItem*> aitems_;
+    std::vector<LocationItem*> locItems_;
+
+    /**
+     * @brief considerSendingFromHand tries to send an agent from hand to a random location
+     * @pre -
+     * @post if conditions are met,  action_ is set to sendagentaction (hand -> location)
+     */
+    int considerSendingFromHand();
+    /**
+     * @brief considerWithdrawing looks if there are any agents which have councilor cards
+     * @pre -
+     * @post if conditions are met, action_ is sent to withdrawaction
+     */
+    int considerWithdrawing();
+    /**
+     * @brief considerSmartMoving looks if there are any agents in locations which have adjacent locations with demands the agent can fulfill
+     * @pre locations have neighbours
+     * @post if conditions are met, sets action_ to sendagentaction (location -> location)
+     */
+    int considerSmartMoving();
+    /**
+     * @brief considerRandomMoving looks if there are any agents in locations, and rolls a chance to move based on their influence
+     * @pre locations have neighbours
+     * @post if conditions are met, sets action_ to sendagentaction (location -> location)
+     */
+    int considerRandomMoving();
+
+
+    // Stores consideration functions. When the AI needs to generate a new action, it goes this in order (from first to last)
+    // which means that you can modify action "priority" by changing its index
+    std::vector< int (AiControl::*)()> considerationsVector = {
+            &AiControl::considerSendingFromHand,
+            &AiControl::considerWithdrawing,
+            &AiControl::considerSmartMoving,
+            &AiControl::considerRandomMoving
+    };
+
+
+
+
+
 };
 } // Interface
 #endif // AICONTROL_HH
